3a54ed2446645f69b2f9f5e513ac7dec1a1cb94b
godsparrow <akilesh.sutharsan@gmail.com>
2026-02-17 20:03:57 -0500

    Update game.js

--- js/core/game.js
+++ js/core/game.js
@@ -1,7 +1,1 @@
-window.onload = function () {
-    const canvas = document.getElementById("game");
-    const ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "red";
-    ctx.fillRect(100, 100, 400, 200);
-};
+alert("GITHUB VERSION UPDATED");
